e5172fb504059cbb39dd4d5ab1443058963ad5b0
Mostafa Abdelhamid <mhsmustafa84@gmail.com>
2026-08-01 18:01:14 +0300

    fix: use leading slash in router basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="react-routes-forge-poc">
+    <BrowserRouter basename="/react-routes-forge-poc">
       <App />
     </BrowserRouter>
   </StrictMode>,
